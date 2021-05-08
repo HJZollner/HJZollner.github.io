82f2a77af396b28f87c7af2fadaeb4b2397f1fd8
Helge <47348963+HJZollner@users.noreply.github.com>
2021-05-08 15:50:13 -0400

    SpecVis Release
    Release SpecVis

---
+++ _posts/2021-05-08-SpecVis-Release-#04.markdown
@@ -0,0 +1,12 @@
+---
+layout: post
+title:  "SpecVis v.1.0.0 Release"
+date:   2021-05-08 15:00:00 -0500
+categories: SpecVis update
+tags: SpecVis open-source open-science
+---
+I;m happy to announce the Release of SpecVis v.1.0.0 today. This R repository includes a number of functions to create reproducible and information rich plots for transparent data reporting.
+
+The functions are freely avaialbe on the [SpecVis GitHub](https://github.com/HJZollner/SpecVis/releases).
+
+If you have any suggestions please contact me,
--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Hi! I’ll use this blog to include announce new changes to Osprey and SpecVis as well as recent updates iabout my research. Helge ","categories": ["jekyll","update"],
         "tags": [],
         "url": "/jekyll/update/2021/02/20/Osprey-Updates-02.html"
+      },{
+        "title": "MRSinMRS markdown in Osprey",
+        "excerpt":"Dear MRS community, we are happy to announce a new module in Osprey that automatically creates a table following the consensus recommendations. The output format is markdown which allows opening in any text editor independently of your OS. The final document can easily be used to generate a PDF to...","categories": ["Osprey","update"],
+        "tags": ["Osprey","open-source","open-science"],
+        "url": "/osprey/update/2021/04/26/Osprey-Updates-03.html"
       }]
