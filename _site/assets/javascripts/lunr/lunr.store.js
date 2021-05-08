10eb63e0a4cd77c5e920458aab9f25687a283948
Helge <47348963+HJZollner@users.noreply.github.com>
2021-05-08 16:02:35 -0400

    Add Image
    Hopefully an image

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Dear MRS community, we are happy to announce a new module in Osprey that automatically creates a table following the consensus recommendations. The output format is markdown which allows opening in any text editor independently of your OS. The final document can easily be used to generate a PDF to...","categories": ["Osprey","update"],
         "tags": ["Osprey","open-source","open-science"],
         "url": "/osprey/update/2021/04/26/Osprey-Updates-03.html"
+      },{
+        "title": "SpecVis v.1.0.0 Release",
+        "excerpt":"I’m happy to announce the release of SpecVis v.1.0.0 today. This R repository includes a number of functions to create reproducible and information rich plots for transparent data reporting. The functions are freely avaialbe on the SpecVis GitHub. If you have any suggestions please contact me, ","categories": ["SpecVis","update"],
+        "tags": ["SpecVis","open-source","open-science"],
+        "url": "/specvis/update/2021/05/08/SpecVis-Release-04.html"
       }]
